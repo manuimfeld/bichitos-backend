9f27dc46c2ab78f61285bb5376ce4fc474b3111b
manuimfeld <manu_jadir@hotmail.com>
2024-07-29 15:33:28 -0300

    add experimental authorization role for use routes

--- routes/sales.js
+++ routes/sales.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const salesController = require("../controllers/sales");
+const { authenticateJWT, authorizeRole } = require("../utils/jwt");
 const router = express.Router();
 
-router.get("/sales", salesController.getAllSales);
+router.get(
+  "/sales",
+  authenticateJWT,
+  authorizeRole("admin"),
+  salesController.getAllSales
+);
 router.post("/sales", salesController.createSale);
 
 module.exports = router;
