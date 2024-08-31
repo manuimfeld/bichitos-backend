e947e020580fee024caa116977e691d2769d4c0a
manuimfeld <manu_jadir@hotmail.com>
2024-08-31 13:38:25 -0300

    migrate to typescript

--- app.ts
+++ app.ts
@@ -8,15 +8,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/* const salesRoutes = require("./routes/sales");
+const salesRoutes = require("./routes/sales");
 const productsRoutes = require("./routes/products");
 const expensesRoutes = require("./routes/expenses");
-const providersRoutes = require("./routes/providers"); */
+const providersRoutes = require("./routes/providers");
 
-/* app.use("/api", salesRoutes); */
+app.use("/api", salesRoutes);
 app.use("/api", authRoutes);
-/* app.use("/api", productsRoutes);
+app.use("/api", productsRoutes);
 app.use("/api", expensesRoutes);
-app.use("/api", providersRoutes); */
+app.use("/api", providersRoutes);
 
 app.listen(process.env.API_PORT);
