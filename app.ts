47f0b2f907fe22a354dbb18d7c55e64a22e4f25b
manuimfeld <manu_jadir@hotmail.com>
2024-09-01 00:39:32 -0300

    add comments

--- app.ts
+++ app.ts
@@ -1,16 +1,18 @@
 import cors from "cors";
 import express from "express";
-//Routes
+
+//Import routes
 import salesRoutes from "./routes/sales";
 import authRoutes from "./routes/auth";
 import providersRoutes from "./routes/providers";
 import expensesRoutes from "./routes/expenses";
-//
 
+//Config app
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Declare use routes
 app.use("/api", salesRoutes);
 app.use("/api", authRoutes);
 app.use("/api", expensesRoutes);
