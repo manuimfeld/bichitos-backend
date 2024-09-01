891ab9256519efa9a78adfcb05fd541b57324dda
manuimfeld <manu_jadir@hotmail.com>
2024-09-01 01:13:58 -0300

    remove dist folder

--- src/dist/routes/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../controllers/auth");
-const router = (0, express_1.Router)();
-router.post("/auth/login", auth_1.postAuth);
-router.post("/auth/register", auth_1.postRegister);
-exports.default = router;
