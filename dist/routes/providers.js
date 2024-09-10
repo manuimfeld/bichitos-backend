2a902a6cee7baa89ed7a53c422dc591e56ffee27
manuimfeld <manu_jadir@hotmail.com>
2024-09-10 01:12:08 -0300

    remove dist

--- dist/routes/providers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const jwt_1 = require("../utils/jwt");
-const providers_1 = require("../controllers/providers");
-const router = (0, express_1.Router)();
-router.get("/providers", jwt_1.authenticateJWT, providers_1.getAllProviders);
-exports.default = router;
